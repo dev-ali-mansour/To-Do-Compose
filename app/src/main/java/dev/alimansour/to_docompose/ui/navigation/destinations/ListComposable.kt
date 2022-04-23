fd6e3cf3cd52a396f0ea7253b1f1baa809c273a5
Ali Mansour <dev.ali.mansour@gmail.com>
2022-04-23 15:21:48 +0200

    Create ListAppBar Composable and DefaultListAppBar Composable.

--- app/src/main/java/dev/alimansour/to_docompose/ui/navigation/destinations/ListComposable.kt
+++ app/src/main/java/dev/alimansour/to_docompose/ui/navigation/destinations/ListComposable.kt
@@ -4,16 +4,17 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import dev.alimansour.to_docompose.ui.screen.list.ListScreen
 import dev.alimansour.to_docompose.util.Constants.LIST_ARGUMENT_KEY
 import dev.alimansour.to_docompose.util.Constants.LIST_SCREEN
 
-fun NavGraphBuilder.listComposable(navigateToTaskScreen: (Int) -> Unit) {
+fun NavGraphBuilder.listComposable(navigateToTaskScreen: (taskId: Int) -> Unit) {
     composable(
         route = LIST_SCREEN,
         arguments = listOf(navArgument(LIST_ARGUMENT_KEY) {
             type = NavType.StringType
         })
     ) {
-
+        ListScreen(navigateToTaskScreen = navigateToTaskScreen)
     }
 }
---
+++ app/src/main/java/dev/alimansour/to_docompose/ui/screen/list/ListAppBar.kt
@@ -0,0 +1,140 @@
+package dev.alimansour.to_docompose.ui.screen.list
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import dev.alimansour.to_docompose.R
+import dev.alimansour.to_docompose.data.model.Priority
+import dev.alimansour.to_docompose.ui.component.PriorityItem
+import dev.alimansour.to_docompose.ui.theme.LARGE_PADDING
+import dev.alimansour.to_docompose.ui.theme.topAppBarBackgroundColor
+import dev.alimansour.to_docompose.ui.theme.topAppBarContentColor
+
+@Composable
+fun ListAppBar() {
+    DefaultListAppBar(
+        onSearchClicked = {},
+        onSortClicked = {},
+        onDeleteClicked = {})
+}
+
+@Composable
+fun DefaultListAppBar(
+    onSearchClicked: () -> Unit,
+    onSortClicked: (Priority) -> Unit,
+    onDeleteClicked: () -> Unit
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text = "Tasks",
+                color = MaterialTheme.colors.topAppBarContentColor
+            )
+        },
+        actions = {
+            ListAppBarActions(
+                onSearchClicked = onSearchClicked,
+                onSortClicked = onSortClicked,
+                onDeleteClicked = onDeleteClicked
+            )
+        },
+        backgroundColor = MaterialTheme.colors.topAppBarBackgroundColor,
+    )
+}
+
+@Composable
+fun ListAppBarActions(
+    onSearchClicked: () -> Unit,
+    onSortClicked: (Priority) -> Unit,
+    onDeleteClicked: () -> Unit
+) {
+    SearchAction(onSearchClicked = onSearchClicked)
+    SortAction(onSortClicked = onSortClicked)
+    DeleteAllAction(onDeleteClicked = onDeleteClicked)
+}
+
+@Composable
+fun SearchAction(onSearchClicked: () -> Unit) {
+    IconButton(onClick = { onSearchClicked() }) {
+        Icon(
+            imageVector = Icons.Filled.Search,
+            contentDescription = stringResource(R.string.search_action),
+            tint = MaterialTheme.colors.topAppBarContentColor
+        )
+    }
+}
+
+@Composable
+fun SortAction(onSortClicked: (Priority) -> Unit) {
+    var expanded by remember { mutableStateOf(false) }
+
+    IconButton(onClick = { expanded = true }) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_filter_list),
+            contentDescription = stringResource(R.string.sort_action),
+            tint = MaterialTheme.colors.topAppBarContentColor
+        )
+        DropdownMenu(expanded = expanded,
+            onDismissRequest = { expanded = false }) {
+            DropdownMenuItem(onClick = {
+                expanded = false
+                onSortClicked(Priority.LOW)
+            }) {
+                PriorityItem(priority = Priority.LOW)
+            }
+            DropdownMenuItem(onClick = {
+                expanded = false
+                onSortClicked(Priority.HIGH)
+            }) {
+                PriorityItem(priority = Priority.HIGH)
+            }
+            DropdownMenuItem(onClick = {
+                expanded = false
+                onSortClicked(Priority.NONE)
+            }) {
+                PriorityItem(priority = Priority.NONE)
+            }
+        }
+    }
+}
+
+@Composable
+fun DeleteAllAction(onDeleteClicked: () -> Unit) {
+    var expanded by remember { mutableStateOf(false) }
+
+    IconButton(onClick = { expanded = true }) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_vertical_menu),
+            contentDescription = stringResource(R.string.delete_all_action),
+            tint = MaterialTheme.colors.topAppBarContentColor
+        )
+        DropdownMenu(expanded = expanded,
+            onDismissRequest = { expanded = false }) {
+            DropdownMenuItem(onClick = {
+                expanded = false
+                onDeleteClicked()
+            }) {
+                Text(
+                    modifier = Modifier.padding(start = LARGE_PADDING),
+                    text = stringResource(R.string.delete_all_action),
+                    style = MaterialTheme.typography.subtitle2
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DefaultListAppBarPreview() {
+    DefaultListAppBar(
+        onSearchClicked = {},
+        onSortClicked = {},
+        onDeleteClicked = {})
+}

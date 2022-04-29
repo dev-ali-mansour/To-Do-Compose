8c9aa4beff186392a0c42e3c7d2bdbcd387e276e
Ali Mansour <dev.ali.mansour@gmail.com>
2022-04-29 13:02:26 +0200

    Intercept Back button in TaskScreen.

--- app/src/main/java/dev/alimansour/to_docompose/ui/screen/task/TaskContent.kt
+++ app/src/main/java/dev/alimansour/to_docompose/ui/screen/task/TaskContent.kt
@@ -10,7 +10,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import dev.alimansour.to_docompose.R
 import dev.alimansour.to_docompose.data.model.Priority

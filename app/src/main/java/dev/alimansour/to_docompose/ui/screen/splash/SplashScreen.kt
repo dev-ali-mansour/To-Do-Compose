0af46c2c8a5275c4698e7d61fab5494f915a055c
Ali Mansour <dev.ali.mansour@gmail.com>
2022-04-29 14:42:09 +0200

    Extract SplashScreen content to a separate Composable function named Splash and use it in Preview.

--- app/src/main/java/dev/alimansour/to_docompose/ui/screen/splash/SplashScreen.kt
+++ app/src/main/java/dev/alimansour/to_docompose/ui/screen/splash/SplashScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.graphics.DefaultAlpha
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import dev.alimansour.to_docompose.R
 import dev.alimansour.to_docompose.ui.theme.LOGO_HEIGHT
@@ -44,6 +45,11 @@ fun SplashScreen(navigateToListScreen: () -> Unit) {
         delay(SPLASH_SCREEN_DELAY)
         navigateToListScreen()
     }
+    Splash(offsetState = offsetState, alphaState = alphaState)
+}
+
+@Composable
+private fun Splash(offsetState: Dp, alphaState: Float) {
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -68,13 +74,13 @@ else R.drawable.ic_logo_light
 @Preview
 @Composable
 fun SplashScreenPreview() {
-    SplashScreen {}
+    Splash(0.dp, 1f)
 }
 
 @Preview
 @Composable
 fun SplashScreenPreviewDark() {
     ToDoComposeTheme(darkTheme = true) {
-        SplashScreen {}
+        Splash(0.dp, 1f)
     }
 }
